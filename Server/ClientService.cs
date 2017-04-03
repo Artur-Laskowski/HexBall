39a279aa327f659948acfa31370d8e7c2f684469
zjomz <zjomz@DESKTOP-GMTN5OR>
2017-04-03 22:51:46 +0200

    -added server socket service -added client handling service

---
+++ Server/ClientService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ClientService
+    {
+        TcpClient clientSocket { get; set; }
+        int number { get; set; } //id?
+
+        public ClientService(TcpClient inClientSocket, int nmbr)
+        {
+            this.clientSocket = inClientSocket;
+            this.number = nmbr;
+            //
+                // init player on game object
+            //
+            Thread clientThread = new Thread(ClientHandler);
+            clientThread.Start();
+        }
+
+        public void ClientHandler()
+        {
+            byte[] bytesIn = new byte[400096];
+            byte[] bytesOut = new byte[8192];
+            String zxc = "blasladlsadasdjaiosjdaoisjdsaodhsiudfasudfha";
+            while (true)
+            {
+                try
+                {
+                    NetworkStream networkStream = this.clientSocket.GetStream();
+                    networkStream.Read(bytesIn, 0, (int)clientSocket.ReceiveBufferSize);
+                    //
+                    //deserialization
+                    //add event with this player id to game object
+                    //serialize map to byte array
+                    //send back to client
+                    //
+                    Serializer.ObjectToByteArray(zxc).CopyTo(bytesOut, 0);
+                    networkStream.Write(bytesOut, 0, bytesOut.Count());
+                    networkStream.Flush();
+                }catch(Exception e)
+                {
+                    break;
+                }
+                
+            }
+        }
+
+        
+    }
+}
---
+++ Server/SocketService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class SocketService
+    {
+        TcpListener serverSocket { get; set; }
+        TcpClient clientSocket { get; set; }
+
+        int connections { get; set; } = 0;
+
+        public SocketService(int port, string ip)
+        {
+            //server socket obj
+            if (String.IsNullOrEmpty(ip))
+            {
+                //localhost
+                this.serverSocket = new TcpListener(System.Net.IPAddress.Parse("127.0.0.1"), port);
+            }
+            else
+            {
+                this.serverSocket = new TcpListener(System.Net.IPAddress.Parse(ip), port);
+            }
+
+
+            //client socket obj
+            this.clientSocket = new TcpClient();
+        }
+
+        public void Start()
+        {
+            this.serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started");
+            while (true)
+            {
+                this.clientSocket = serverSocket.AcceptTcpClient();
+                connections++;
+                Console.WriteLine(" >> " + "user " + connections + " connected");
+                new ClientService(this.clientSocket, connections);
+            }
+        }
+
+    }
+}
