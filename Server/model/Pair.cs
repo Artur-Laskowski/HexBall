809c425b663af3ff69823a98967dafc5f13744a5
Szymon Puszcz <szymon1324@gmail.com>
2017-05-31 15:38:17 +0200

    refactor Pair

--- Server/model/Pair.cs
+++ Server/model/Pair.cs
@@ -58,54 +58,5 @@ namespace HexBall
         {
             return Math.Sqrt(Math.Pow(b.First - a.First, 2) + Math.Pow(b.Second - a.Second, 2));
         }
-
-        public bool IsBetween(double size, Tuple<Pair, Pair> pairs)
-        {
-            var points=new Pair[4];
-            points[0]=new Pair(this);
-            points[0].Second -= size;
-
-            points[1] = new Pair(this);
-            points[1].First += size;
-
-            points[2] = new Pair(this);
-            points[2].Second += size;
-
-            points[3] = new Pair(this);
-            points[3].First -= size;
-
-            foreach (var point in points)
-            {
-                if (point.IsBetween(pairs))
-                    return true;
-            }
-            return false;
-        }
-        public bool IsBetween(Tuple<Pair, Pair> pairs)
-        {
-            var begin = pairs.Item1;
-            var end = pairs.Item2;
-            return this > begin && this < end;
-        }
-
-        public static Pair GetCenterOfPairs(Tuple<Pair, Pair> pairs)
-        {
-            var center = new Pair()
-            {
-                First = (pairs.Item1.First + pairs.Item1.First) / 2.0,
-                Second = (pairs.Item1.Second + pairs.Item1.Second) / 2.0
-            };
-            return center;
-        }
-
-        public Pair GetCenter(int size)
-        {
-            var center=new Pair()
-            {
-                First = this.First+size/2.0,
-                Second = this.Second+size/2.0
-            };
-            return center;
-        }
     }
 }
